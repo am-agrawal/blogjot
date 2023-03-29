8bea01c3243aac257ab792e75f078913feba16ed
Aman Agrawal <aman.agrawal@zenarate.com>
2023-03-29 10:34:08 +0530

    add api key to env file

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({ baseURL: 'https://gorest.co.in/public/v2' });
-instance.defaults.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem('token')}`;
+instance.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_API_KEY}`;
 instance.defaults.headers.post['Content-Type'] = 'multipart/form-date';
 
 export default instance;
